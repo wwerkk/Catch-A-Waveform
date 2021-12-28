717de2f2ceb6477a6fcf96320a03217967e5e773
zvk <drawordoodle@gmail.com>
2021-12-28 12:24:03 -0800

    setup and editible package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Catch-A-Waveform',
+      version='1.0',
+      description='Dadabots forked implementation of the paper: "Catch-A-Waveform: Learning to Generate Audio from a Single Short Example" (NeurIPS 2021) by Gal Greshler',
+      author='Zack Zukowski',
+      author_email='thedadabot@gmail.com',
+      url='https://www.github.org/dada-bots/Catch-A-Waveform',
+      packages= ['/', 'models', 'utils'],
+     )
